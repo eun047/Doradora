1499f0a2359db182c9ae6e8d14d1a8e2294ec35c
eun047 <chaeeun.wonchoi@gmail.com>
2026-08-07 23:22:15 +0900

    chore: Doradora 프로젝트 초기 설정

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">Doradora</h1>
+    </main>
+  );
+}
+
+export default App;
